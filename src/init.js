67b746864aba58222f85444d9029f937929dfa52
Aimee Rosato <aimee.rosato@gmail.com>
2016-01-18 22:51:21 -0500

    fixed instantiation bug in init

--- src/init.js
+++ src/init.js
@@ -5,7 +5,9 @@ $(document).ready(function(){
     /* This function sets up the click handlers for the create-dancer
      * buttons on dancefloor.html. You should only need to make one small change to it.
      * As long as the "data-dancer-maker-function-name" attribute of a
-     * class="addDancerButton" DOM node matches one of the names of the
+     * class="addDancerButton" DOM node 
+
+     matches one of the names of the
      * maker functions available in the global scope, clicking that node
      * will call the function to make the dancer.
      */
@@ -16,16 +18,14 @@ $(document).ready(function(){
      * to the stage.
      */
     var dancerMakerFunctionName = $(this).data("dancer-maker-function-name");
-
     // get the maker function for the kind of dancer we're supposed to make
-    var dancerMakerFunction = window[dancerMakerFunctionName];
-
+    var dancerMakerFunction =  window[dancerMakerFunctionName];
     // make a dancer with a random position
-
-    var dancer = dancerMakerFunction(
+    // needed new keyword when instantiating 
+    var dancer = new dancerMakerFunction(
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
-      Math.random() * 1000
+      Math.random()*1000
     );
 
     window.dancers.push(dancer);
